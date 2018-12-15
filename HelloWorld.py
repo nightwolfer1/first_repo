94d782fa7280386ef5f2039bd6d23137a6ea29ff
nightwolfer1 <danielnilsson2002@hotmail.com>
2018-12-15 17:08:54 +0100

    just testing if i can commit numerical-mooc and changes to HelloWorld

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,3 +1,3 @@
 print("Greetings Earth! we come in peace.")
-
+print("adding this to see changes")
 
---
+++ numerical-mooc/myscripts/phug.py
@@ -0,0 +1,3 @@
+from IPython.core.display import HTML
+css_file = '../../styles/numericalmoocstyle.css'
+HTML(open(css_file, 'r').read())
